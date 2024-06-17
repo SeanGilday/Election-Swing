18ecf9131a3e1e7be17faf1edc2fc76662b61c6b
Sean Gilday <spgilday19@gmail.com>
2024-06-17 17:32:26 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,80 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from openpyxl import load_workbook
+from openpyxl.utils import get_column_letter
+import requests
+import csv
+import json
+import requests
+from bs4 import BeautifulSoup
+from openpyxl import load_workbook
+from openpyxl.utils import get_column_letter
+
+class helloHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        workbk = load_workbook(filename='county_Feb22.xlsx')
+        ws = workbk.active
+
+        excelStats = []
+
+        last_column = len(list(ws.columns))
+        last_row = len(list(ws.rows))
+        keys = ws[1]
+
+        HOST = "https://www.elections.ny.gov/2022BallotProposal.html"
+        URLback = "/EnrollmentCounty.html"
+        pageback = requests.get(HOST)
+
+        soup = BeautifulSoup(pageback.content, "html.parser")
+
+        results = soup.find_all("div", id="midContent")
+        print(results, "------------------\n")
+
+        for row in range(2, last_row + 1):
+            temp = dict()
+            excelStats.append(temp)
+            for col in range(1, len(ws[row])):
+                temp[keys[col].value] = cellValue = ws[row][col].value
+                cellValue = ws[row][col].value
+
+        results = str(results)
+
+        data = {"title": "Test", "description": results, "rows": excelStats}
+        package = json.dumps(data)
+
+        self.send_response(200)
+        self.send_header('content-type', 'text/html')
+        self.end_headers()
+        #Arg my list JSON
+        self.wfile.write(package.encode())
+        print(package)
+
+
+def main():
+
+    workbk = load_workbook(filename='county_Feb22.xlsx')
+    ws = workbk.active
+
+    excelStats = []
+
+    last_column = len(list(ws.columns))
+    last_row = len(list(ws.rows))
+    keys = ws[1]
+
+    for row in range(2, last_row + 1):
+        temp = dict()
+        excelStats.append(temp)
+        for col in range(1, len(ws[row])):
+            temp[keys[col].value] = cellValue = ws[row][col].value
+            cellValue = ws[row][col].value
+    data = json.dumps(excelStats)
+    print(data)
+
+    PORT = 8080
+    server = HTTPServer(('', PORT), helloHandler)
+    print('Server running on port %s' % PORT)
+    server.serve_forever()
+
+
+if __name__ == "__main__":
+    main()
---
+++ script.js
@@ -0,0 +1,21 @@
+var jcontent = {
+    "firstName": "John",
+    "lastName": "Smith"
+  }
+  
+  var output = document.getElementById('output');
+  output.innerHTML = 'COUNTY  STATUS  DEM  REP  CON  WOR  OTH  BLANK  TOTAL'
+  
+  var active = document.getElementById('active');
+  active.innerHTML = 'Albany  Active  98146  34869  3436  892  8393  48655  194391'
+  
+  var inactive = document.getElementById('inactive');
+  inactive.innerHTML = 'Albany  Inactive  8816  3007  267  94  1025  4809  18018'
+  
+  var total = document.getElementById('total');
+  total.innerHTML = 'Albany  Total  106962  37876  3703  986  9418  53464  212409'
+  
+  
+  /*
+   [{"COUNTY": "Albany ", "STATUS": "Active", "DEM": 98146, "REP": 34869, "CON": 3436, "WOR": 892, "OTH": 8393, "BLANK": 48655, "TOTAL": 194391}, {"COUNTY": "Albany ", "STATUS": "Inactive", "DEM": 8816, "REP": 3007, "CON": 267, "WOR": 94, "OTH": 1025, "BLANK": 4809, "TOTAL": 18018}, {"COUNTY": "Albany ", "STATUS": "Total", "DEM": 106962, "REP": 37876, "CON": 3703, "WOR": 986, "OTH": 9418, "BLANK": 53464, "TOTAL": 212409}]
+  */
